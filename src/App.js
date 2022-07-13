f4932673fcb0aa38dcd4e296f2f50617073194d4
Erik Dell <edellenator@gmail.com>
2022-07-13 01:32:03 -0700

    contact section renders and passes all tests

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import About from './components/About';
 import Nav from './components/Nav'
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
@@ -23,12 +25,20 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <Gallery 
-          currentCategory={currentCategory}
-        ></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery 
+              currentCategory={currentCategory}
+            ></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
